1cd303ff073fd42e5625af500aed057a72d08212
Teo Klestrup Röijezon <teo@nullable.se>
2014-11-05 16:50:43 +0100

    Added a function for using a Yesod context without being inside a MonadHandler

--- battlenet-yesod/BattleNet/Yesod.hs
+++ battlenet-yesod/BattleNet/Yesod.hs
@@ -1,4 +1,4 @@
-module BattleNet.Yesod(YesodBattleNet(..), yesodBnet) where
+module BattleNet.Yesod(YesodBattleNet(..), yesodBnetWithApp, yesodBnet) where
 
 import BattleNet
 import Yesod.Core
@@ -9,9 +9,11 @@ class YesodBattleNet app where
     battleNetKey :: app -> BattleNetApiKey
     battleNetHttp :: app -> Manager
 
+yesodBnetWithApp :: (YesodBattleNet app) => app -> (Manager -> BattleNetApiKey -> IO a) -> IO a
+yesodBnetWithApp yesod endpoint =
+        endpoint manager key
+    where key = battleNetKey yesod
+          manager = battleNetHttp yesod
+
 yesodBnet :: (MonadHandler m, YesodBattleNet (HandlerSite m)) => (Manager -> BattleNetApiKey -> IO a) -> m a
-yesodBnet endpoint = do
-    yesod <- getYesod
-    let key = battleNetKey yesod
-        manager = battleNetHttp yesod
-    liftIO $ endpoint manager key
+yesodBnet endpoint = getYesod >>= liftIO . (flip yesodBnetWithApp endpoint)
