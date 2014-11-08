69f2b2d7fec15b4515b45fed22b6cecf2f5e93b2
Teo Klestrup Röijezon <teo@nullable.se>
2014-11-08 01:42:42 +0100

    Guild members endpoint

--- BattleNet/WoW.hs
+++ BattleNet/WoW.hs
@@ -37,9 +37,37 @@ instance FromJSON UserCharactersWrapper where
     parseJSON (Object v) = UserCharactersWrapper <$> v .: "characters"
     parseJSON _ = mzero
 
+data WoWGuildMemberInfo = WoWGuildMemberInfo
+    { memberName :: Text
+    , memberRealm :: Text
+    , memberRank :: Int
+    }
+
+instance FromJSON WoWGuildMemberInfo where
+    parseJSON (Object v) = do
+        member <- v .: "character"
+        memberName <- member .: "name"
+        memberRealm <- member .: "realm"
+        memberRank <- v .: "rank"
+        return WoWGuildMemberInfo
+            { memberName = memberName
+            , memberRealm = memberRealm
+            , memberRank = memberRank
+            }
+
+newtype GuildMemberWrapper = GuildMemberWrapper [WoWGuildMemberInfo]
+
+instance FromJSON GuildMemberWrapper where
+    parseJSON (Object v) = GuildMemberWrapper <$> v .: "members"
+    parseJSON _ = mzero
+
 character :: Text -> Text -> Manager -> BattleNetApiKey -> IO WoWCharacterInfo
-character name realm = apiEndpoint ["wow", "character", name, realm] []
+character realm name = apiEndpoint ["wow", "character", realm, name] []
 
 userCharacters :: Text -> Manager -> BattleNetApiKey -> IO [WoWCharacterInfo]
 userCharacters accessToken manager key = unwrapChars <$> apiEndpoint ["wow", "user", "characters"] [("access_token", accessToken)] manager key
     where unwrapChars (UserCharactersWrapper chars) = chars
+
+guildMembers :: Text -> Text -> Manager -> BattleNetApiKey -> IO [WoWGuildMemberInfo]
+guildMembers realm name manager key = unwrapMembers <$> apiEndpoint ["wow", "guild", realm, name] [("fields", "members")] manager key
+    where unwrapMembers (GuildMemberWrapper chars) = chars
